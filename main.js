21927b3e63b12110439f8e703b816b71e8b65426
Daniel <daniel.frimu@hoculus.com>
2026-06-24 11:17:46 +0300

    feat: Query products

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 const BASE_URL = 'https://shopify-app-express-server.vercel.app';
 
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    fetch(BASE_URL)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            document.body.insertAdjacentHTML('beforeend', `<pre>${JSON.stringify(data, null, 2)}</pre>`)
-        })
-        .catch(error => {
-            console.error(error);
-            document.body.insertAdjacentHTML('beforeend', `<pre>${JSON.stringify(error, null, 2)}</pre>`)
-        });
-});
+fetch(`${BASE_URL}/products`)
+    .then(res => res.json())
+    .then(console.log)
+    .catch(console.error);
